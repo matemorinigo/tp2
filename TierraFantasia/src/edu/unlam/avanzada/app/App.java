db7b2afab5c7395dc79ff38febdc9171618bbab0
Santiago Alvarez <alvarezsanjavier@gmail.com>
2024-11-08 22:27:47 -0300

    actualizo main

--- TierraFantasia/src/edu/unlam/avanzada/app/App.java
+++ TierraFantasia/src/edu/unlam/avanzada/app/App.java
@@ -1,5 +1,33 @@
 package edu.unlam.avanzada.app;
 
-public class App {
+import edu.unlam.avanzada.entidades.Ejercito;
 
+public class App {
+	public static void main(String[] args) {
+		juego();
+	}
+	
+	
+	public static void juego() {
+		
+		Ejercito ej1 = new Ejercito(100, "wrives", "propio");
+		Ejercito ej2 = new Ejercito(200, "reralopes", "enemigo");
+		batalla(ej1,ej2);
+		
+	}
+	
+	public static void batalla(Ejercito ejercito1, Ejercito ejercito2) {
+		while(!ejercito1.sinUnidades() && !ejercito2.sinUnidades()) {
+			
+			ejercito1.atacar(ejercito2);
+			
+			if(!ejercito2.sinUnidades())
+				ejercito2.atacar(ejercito1);
+		}
+		if(!ejercito1.sinUnidades())
+			System.out.println("Gano ejercito 1");
+		else
+			System.out.println("Gano ejercito 2");
+	}
+	
 }
